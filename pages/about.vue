f07583c6072c59e9a0555c3dce86304f9436b9cf
JasonLinn <cheng11220@gmail.com>
2018-01-07 00:36:05 +0800

    init scss

---
+++ pages/about.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="page" id="page-about">
+  <div class="bannerbox">
+    <h1 class="title wow fadeInUpCust"><span>溫馨服務情</span><span>窩心衿日林</span></h1>
+  </div>
+  <div class="maincontent">
+    <h2><span class="wow fadeInUp"> 關於我們</span></h2>
+    <div class="aboutbox wow fadeInUp">
+      <div class="container"><span>衿日林民宿擁有得天獨厚的地理環境，位於冬山河親水公園的正對面。</span><span>在這裡，不論是曙光微露或是夕陽西下，您都能夠近距離的欣賞到冬山河的不同面貌。</span><span>從窗外望去，映入眼簾一片綠油油的稻田，它總是夏天最美麗的一幅畫。</span><span>步行於冬山河畔，當夏日晚風徐徐吹撫，生活就是如此愜意。</span></div>
+    </div>
+    <ul class="picbox">
+      <li>
+        <div class="pic wow fadeInUp"><img class="rwd-img" src="images/about/about3.jpg" alt="衿日林民宿-冬山河親水公園"/></div>
+      </li>
+      <li>
+        <div class="pic wow fadeInUp"><img class="rwd-img" src="images/about/about2.jpg" alt="衿日林民宿-冬山河"/></div>
+      </li>
+      <li>
+        <div class="pic wow fadeInUp"><img class="rwd-img" src="images/about/about6.jpg" alt="衿日林民宿-綠油油的稻田"/></div>
+      </li>
+    </ul>
+    <div class="aboutbox wow fadeInUp">
+      <div class="container"><span>得天獨厚的地理環境黎明的晨曦　</span><span>黃昏的夕陽帶領全家人來到 衿日林民宿</span><span>您的另一個舒適的家享受身心靈SPA</span></div>
+    </div>
+    <ul class="picbox">
+      <li>
+        <div class="pic wow fadeInUp"><img class="rwd-img" src="images/about/about4.jpg" alt="衿日林民宿-心靈SPA"/></div>
+      </li>
+      <li>
+        <div class="pic wow fadeInUp"><img class="rwd-img" src="images/about/about5.jpg" alt="衿日林民宿-黎明的晨曦"/></div>
+      </li>
+      <li>
+        <div class="pic wow fadeInUp"><img class="rwd-img" src="images/about/about7.jpg" alt="衿日林民宿-黃昏的夕陽"/></div>
+      </li>
+    </ul>
+  </div>
+</div>
+</template>
